9d804fe92a548beb4baf523695006d40bbdf92fa
DigitalQatalyst <josesyanwa@gmail.com>
2025-04-28 10:32:57 +0300

    initial commit

--- src/lib/graphQLClient.ts
+++ src/lib/graphQLClient.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 
 // Define your GraphQL endpoint URL here
-const endpoint = "http://localhost:3012/shop-api";  // Replace with your actual URL
+const endpoint = "https://22af-54-37-203-255.ngrok-free.app/shop-api";  // Replace with your actual URL
 
 // Create a new instance of the GraphQLClient
 const client = new GraphQLClient(endpoint);

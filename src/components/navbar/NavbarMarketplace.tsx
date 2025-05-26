de3ad92da1c3921eff6cced8399664b74b88fc38
DigitalQatalyst <dq.demo.a@digitalqatalyst.com>
2025-05-26 22:11:26 +0300

    update navbar

--- src/components/navbar/NavbarMarketplace.tsx
+++ src/components/navbar/NavbarMarketplace.tsx
@@ -13,7 +13,6 @@ import Typography, { Span } from "../Typography";
 import Categories from "../categories/Categories";
 
 import StyledNavbar from "./marketStyles";
-import navbarNavigations from "@data/navbarNavigations";
 
 // ============================================================== 
 interface Nav {
@@ -28,116 +27,6 @@ type NavbarProps = { navListOpen?: boolean };
 // ==============================================================
 
 export default function Navbar({ navListOpen }: NavbarProps) {
-  const renderNestedNav = (list: any[], isRoot = false) => {
-    return list?.map((nav: Nav) => {
-      if (isRoot) {
-        if (nav.url && nav.extLink) {
-          return (
-            <NavLink
-              href={nav.url}
-              key={nav.title}
-              target="_blank"
-              className="nav-link"
-              rel="noopener noreferrer"
-            >
-              {nav.badge ? (
-                <Badge style={{ marginRight: "0px" }} title={nav.badge}>
-                  {nav.title}
-                </Badge>
-              ) : (
-                <FlexBox alignItems="center"> {/* Place the text first */}
-                  <Span className="nav-link">{nav.title}</Span>
-                  <img src="/assets/images/mzn_logos/dropdown.svg" alt="Dropdown Icon" className="dropdown-icon" />
-                </FlexBox>
-              )}
-            </NavLink>
-          );
-        }
-
-        if (nav.child) {
-          return (
-            <FlexBox
-              className="root"
-              position="relative"
-              flexDirection="column"
-              alignItems="center"
-              key={nav.title}
-            >
-              {nav.badge ? (
-                <Badge title={nav.badge}>{nav.title}</Badge>
-              ) : (
-                <FlexBox alignItems="center"> {/* Place the text first */}
-                  <Span className="nav-link">{nav.title}</Span>
-                  <img src="/assets/images/mzn_logos/dropdown.svg" alt="Dropdown Icon" className="dropdown-icon" />
-                </FlexBox>
-              )}
-              <div className="root-child">
-                <Card borderRadius={8} mt="1.25rem" py="0.5rem" boxShadow="large" minWidth="230px">
-                  {renderNestedNav(nav.child)}
-                </Card>
-              </div>
-            </FlexBox>
-          );
-        }
-
-        if (nav.url) {
-          return (
-            <NavLink className="nav-link" href={nav.url} key={nav.title}>
-              {nav.badge ? (
-                <Badge style={{ marginRight: "0px" }} title={nav.badge}>
-                  {nav.title}
-                </Badge>
-              ) : (
-                <FlexBox alignItems="center"> {/* Place the text first */}
-                  <Span className="nav-link">{nav.title}</Span>
-                  <img src="/assets/images/mzn_logos/dropdown.svg" alt="Dropdown Icon" className="dropdown-icon" />
-                </FlexBox>
-              )}
-            </NavLink>
-          );
-        }
-      } else {
-        if (nav.url) {
-          return (
-            <NavLink href={nav.url} key={nav.title}>
-              <MenuItem>
-                <FlexBox alignItems="center"> {/* Place the text first */}
-                  <Span className="nav-link">{nav.title}</Span>
-                  <img src="/assets/images/mzn_logos/dropdown.svg" alt="Dropdown Icon" className="dropdown-icon" />
-                </FlexBox>
-              </MenuItem>
-            </NavLink>
-          );
-        }
-
-        if (nav.child) {
-          return (
-            <Box className="parent" position="relative" minWidth="230px" key={nav.title}>
-              <MenuItem
-                color="gray.700"
-                style={{ display: "flex", justifyContent: "space-between" }}
-              >
-                <FlexBox alignItems="center"> {/* Place the text first */}
-                  <Span className="nav-link">{nav.title}</Span>
-                  <img src="/assets/images/mzn_logos/dropdown.svg" alt="Dropdown Icon" className="dropdown-icon" />
-                </FlexBox>
-                <Icon size="8px" defaultcolor="currentColor">
-                  right-arrow
-                </Icon>
-              </MenuItem>
-
-              <Box className="child" pl="0.5rem">
-                <Card py="0.5rem" borderRadius={8} boxShadow="large" minWidth="230px">
-                  {renderNestedNav(nav.child)}
-                </Card>
-              </Box>
-            </Box>
-          );
-        }
-      }
-    });
-  };
-
   return (
     <StyledNavbar>
       <Container height="100%" display="flex" alignItems="center" justifyContent="space-between">
@@ -160,7 +49,7 @@ export default function Navbar({ navListOpen }: NavbarProps) {
               lineHeight="26px"
               color="#002180"
             >
-              All Marketplaces
+              Explore
             </Typography>
 
             <Icon className="dropdown-icon" variant="small">
@@ -169,7 +58,19 @@ export default function Navbar({ navListOpen }: NavbarProps) {
           </Button>
         </Categories>
 
-        <FlexBox style={{ gap: 32 }}>{renderNestedNav(navbarNavigations, true)}</FlexBox>
+        {/* Hardcoded Navigation Links */}
+        <FlexBox style={{ gap: 32 }}>
+          <NavLink className="nav-link" href="#">
+            <FlexBox alignItems="center">
+              <Span className="nav-link">Business in AbuDhabi</Span>
+            </FlexBox>
+          </NavLink>
+          <NavLink className="nav-link" href="#">
+            <FlexBox alignItems="center">
+              <Span className="nav-link">Help Centre</Span>
+            </FlexBox>
+          </NavLink>
+        </FlexBox>
 
         {/* Search Icon, Sign In, and Sign Up Buttons */}
         <FlexBox alignItems="center" style={{ gap: "15px" }}>

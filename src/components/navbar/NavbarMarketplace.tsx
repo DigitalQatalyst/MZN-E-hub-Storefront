a4dd567926ac0e82564d3a57f30420915d9e60e3
DigitalQatalyst <dq.demo.a@digitalqatalyst.com>
2025-06-10 18:32:47 +0300

    adjust navbar style

--- src/components/navbar/NavbarMarketplace.tsx
+++ src/components/navbar/NavbarMarketplace.tsx
@@ -30,63 +30,60 @@ export default function Navbar({ navListOpen }: NavbarProps) {
   return (
     <StyledNavbar>
       <Container height="100%" display="flex" alignItems="center" justifyContent="space-between">
-        {/* Logo Section */}
-        <Box className="navbar-logo">
-          <img src="/assets/images/tab_bar/Subtract.svg" alt="MZN Enterprise Hub" height="40px" />
-        </Box>
-
-        {/* Categories Section */}
-        <Categories open={navListOpen}>
-          <Button width="278px" height="40px" bg="body.default" variant="text">
-            <Icon>categories</Icon>
-            <Typography
-              ml="10px"
-              flex="1 1 0"
-              fontFamily='"Open Sans", sans-serif'
-              fontSize="16px"
-              fontStyle="normal"
-              fontWeight="600"
-              lineHeight="26px"
-              color="#002180"
-            >
-              Explore
-            </Typography>
-
-            <Icon className="dropdown-icon" variant="small">
-              chevron-right
-            </Icon>
-          </Button>
-        </Categories>
-
-        {/* Hardcoded Navigation Links */}
-        <FlexBox style={{ gap: 32 }}>
-          <NavLink className="nav-link" href="#">
-            <FlexBox alignItems="center">
-              <Span className="nav-link">Business in AbuDhabi</Span>
-            </FlexBox>
-          </NavLink>
-          <NavLink className="nav-link" href="#">
-            <FlexBox alignItems="center">
-              <Span className="nav-link">Help Centre</Span>
-            </FlexBox>
-          </NavLink>
+        {/* Logo and Categories Section */}
+        <FlexBox alignItems="center" style={{ gap: "25px" }}>
+          <Box className="navbar-logo">
+            <img src="/assets/images/tab_bar/Subtract.svg" alt="MZN Enterprise Hub" height="35px" />
+          </Box>
+          <Categories open={navListOpen}>
+            <Button width="280px" height="35px" bg="body.default" variant="text" style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "0 10px" }}>
+              <FlexBox alignItems="center">
+                <Icon>categories</Icon>
+                <Typography
+                  ml="4px"
+                  flex="0 0 auto"
+                  fontFamily='"Open Sans", sans-serif'
+                  fontSize="14px"
+                  fontStyle="normal"
+                  fontWeight="600"
+                  lineHeight="22px"
+                  color="#002180"
+                >
+                  Explore
+                </Typography>
+              </FlexBox>
+              <Icon className="dropdown-icon" variant="small">
+                chevron-right
+              </Icon>
+            </Button>
+          </Categories>
         </FlexBox>
 
-        {/* Search Icon, Sign In, and Sign Up Buttons */}
-        <FlexBox alignItems="center" style={{ gap: "15px" }}>
-          {/* Search Icon (Replace with your actual SVG) */}
-          <Box className="search-icon" style={{ cursor: "pointer" }}>
-            <img src="/assets/images/logos/search.svg" alt="Search" height="24px" />
-          </Box>
-
-          {/* Sign In & Sign Up Buttons */}
-          <Button className="sign-in-btn" variant="outlined">
-            Sign In
-          </Button>
-
-          <Button className="sign-up-button" variant="contained">
-            Sign Up
-          </Button>
+        {/* Navigation Links and Right-Side Elements */}
+        <FlexBox alignItems="center" style={{ gap: "20px" }}>
+          <FlexBox style={{ gap: 16 }}>
+            <NavLink className="nav-link" href="#">
+              <FlexBox alignItems="center">
+                <Span className="nav-link">Business in AbuDhabi</Span>
+              </FlexBox>
+            </NavLink>
+            <NavLink className="nav-link" href="#">
+              <FlexBox alignItems="center">
+                <Span className="nav-link">Help Centre</Span>
+              </FlexBox>
+            </NavLink>
+          </FlexBox>
+          <FlexBox alignItems="center" style={{ gap: "20px" }}>
+            <Box className="search-icon" style={{ cursor: "pointer" }}>
+              <img src="/assets/images/logos/search.svg" alt="Search" height="24px" />
+            </Box>
+            <Button className="sign-in-btn" variant="outlined">
+              Sign In
+            </Button>
+            <Button className="sign-up-button" variant="contained">
+              Sign Up
+            </Button>
+          </FlexBox>
         </FlexBox>
       </Container>
     </StyledNavbar>

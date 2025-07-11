10014e1428fb969b78b4f7a5cae9b5964936dce8
digitalqatalyst <dq.demo.a@digitalqatalyst.com>
2025-07-11 03:54:03 +0300

    updated login and signup

--- src/components/navbar/Navbar1.tsx
+++ src/components/navbar/Navbar1.tsx
@@ -17,6 +17,7 @@ import SignInModal from "../SignInModal";
 import StyledNavbar from "./styles";
 import { useModal } from "@context/ModalContext";
 import SignUpModal from "../JoinModal";
+import { useMsal, AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
 
 // Updated navbarNavigations data
 const navbarNavigations = [
@@ -46,6 +47,19 @@ type NavbarProps = { navListOpen?: boolean };
 
 export default function Navbar({ navListOpen }: NavbarProps) {
   const { open, modalType } = useModal();
+  const { instance, accounts } = useMsal();
+
+  function handleLogin() {
+    instance.loginRedirect().catch((e) => {
+      console.log(e);
+    });
+  }
+
+  function handleLogout() {
+    instance.logoutRedirect().catch((e) => {
+      console.log(e);
+    });
+  }
 
   const renderNestedNav = (list: any[], isRoot = false) => {
     return list?.map((nav: Nav) => {
@@ -197,7 +211,12 @@ export default function Navbar({ navListOpen }: NavbarProps) {
           </Box>
 
           {/* Sign In & Sign Up Buttons */}
-          <Button className="sign-in-btn" variant="outlined" mr="10px" ml="10px" onClick={() => open("signin")}>Sign In</Button>
+          <AuthenticatedTemplate>
+            <Button className="sign-in-btn" variant="outlined" mr="10px" ml="10px" onClick={handleLogout}>Logout</Button>
+          </AuthenticatedTemplate>
+          <UnauthenticatedTemplate>
+            <Button className="sign-in-btn" variant="outlined" mr="10px" ml="10px" onClick={handleLogin}>Sign In</Button>
+          </UnauthenticatedTemplate>
           <Button className="sign-up-button" variant="contained" onClick={() => open("signup")}>Sign Up</Button>
           {modalType === "signup" && <SignUpModal />}
           {modalType === "signin" && <SignInModal />}

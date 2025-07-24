8c050f02d606f3b83d65d29bb35ccb41fb1d13d0
DigitalQatalyst <tech@digitalqatalyst.com>
2025-07-24 10:34:41 +0300

    refactor: clean up commented-out code in ProductDetails component

--- src/components/products/ProductDetails.tsx
+++ src/components/products/ProductDetails.tsx
@@ -360,7 +360,7 @@ export default function ProductDetails({ product }: Props) {
           {1}
         </span>
         <TabContainer>
-          {/* <TabButton
+          <TabButton
             active={activeTab === "description"}
             onClick={() => setActiveTab("description")}
           >

bf570472ce7a87b34b5e766e03605428348fe343
DigitalQatalyst <dq.demo.a@digitalqatalyst.com>
2025-08-18 10:48:32 +0300

    `Update postcss.config.js: remove @tailwindcss/postcss and add tailwindcss`

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-// postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
-};
-// This configuration file sets up PostCSS with Tailwind CSS and Autoprefixer.
-// It ensures that Tailwind's utility classes are processed and that vendor prefixes are added to CSS
+}

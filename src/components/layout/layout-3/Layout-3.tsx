8dd945f2735cf849452bbe0086736035fb40b5c6
digitalqatalyst1 <dq.demo.a@digitalqatalyst.com>
2025-08-28 23:28:49 +0300

    updated nav

--- src/components/layout/layout-3/Layout-3.tsx
+++ src/components/layout/layout-3/Layout-3.tsx
@@ -4,11 +4,11 @@ import { PropsWithChildren } from "react";
 
 import AppLayout from "../layout-1";
 import Container from "@component/Container";
-import Navbar from "@component/navbar/Navbar";
+import NavbarMarketplace from "@component/navbar/NavbarMarketplace";
 
 export default function ShopLayout({ children }: PropsWithChildren) {
   return (
-    <AppLayout navbar={<Navbar />}>
+    <AppLayout navbar={<NavbarMarketplace />}>
       <Container my="2rem">{children}</Container>
     </AppLayout>
   );

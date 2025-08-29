fefce97c5c0b3c288925d37e4a8752d093a48d1a
digitalqatalyst1 <dq.demo.a@digitalqatalyst.com>
2025-08-29 11:12:55 +0300

    fix(navbar): clean up category dropdown
    removed chevron right icons from where it wasnt needed

--- src/data/navigations.ts
+++ src/data/navigations.ts
@@ -4,6 +4,7 @@ const navigations = [
     title: "Non-Financial",
     href: "/non-financial-marketplace",
     menuComponent: "MegaMenu1",
+    showCaret: true,
     menuData: {
       categories: [
         {
@@ -122,6 +123,7 @@ const navigations = [
     title: "Financial",
     href: "/product/search/financial-marketplace",
     menuComponent: "MegaMenu1",
+    showCaret: true,
     menuData: {
       categories: [
         {
@@ -178,339 +180,49 @@ const navigations = [
     icon: "local_library",
     title: "Courses",
     href: "/product/search/bikes",
-    menuComponent: "MegaMenu2",
-    menuData: [
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/fashion",
-        megaMenu: "MegaMenu1"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/electronics",
-        megaMenu: 2
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/home&garden",
-        megaMenu: 3
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/bikes",
-        megaMenu: "MegaMenu1"
-      }
-    ]
+    menuComponent: "MegaMenu1",
+    showCaret: false,
+    menuData: null
   },
   {
     icon: "forum",
     title: "Communities",
     href: "/community-marketplace",
     menuComponent: "MegaMenu1",
-    menuData: {
-      categories: [
-        {
-          title: "",
-          href: "/product/search/man-clothes",
-          subCategories: [
-            {
-              title: "",
-              href: "/product/search/shirt",
-              imgUrl: "/assets/images/products/categories/shirt.png"
-            },
-            {
-              title: "",
-              href: "/product/search/t-shirt",
-              imgUrl: "/assets/images/products/categories/t-shirt.png"
-            },
-            {
-              title: "",
-              href: "/product/search/pant",
-              imgUrl: "/assets/images/products/categories/pant.png"
-            },
-            {
-              title: "",
-              href: "/product/search/underwear",
-              imgUrl: "/assets/images/products/categories/t-shirt.png"
-            }
-          ]
-        },
-        {
-          title: "",
-          href: "/product/search/accessories",
-          subCategories: [
-            {
-              title: "",
-              href: "/product/search/belt",
-              imgUrl: "/assets/images/products/categories/belt.png"
-            },
-            {
-              title: "",
-              href: "/product/search/Hat",
-              imgUrl: "/assets/images/products/categories/hat.png"
-            },
-            {
-              title: "",
-              href: "/product/search/Watches",
-              imgUrl: "/assets/images/products/categories/watch.png"
-            },
-            {
-              title: "",
-              href: "/product/search/Sunglasses",
-              imgUrl: "/assets/images/products/categories/sunglass.png"
-            }
-          ]
-        },
-        {
-          title: "",
-          href: "/product/search/shoes",
-          subCategories: [
-            {
-              title: "",
-              href: "/product/search/Sneakers",
-              imgUrl: "/assets/images/products/categories/sneaker.png"
-            },
-            {
-              title: "",
-              href: "/product/search/Sandals",
-              imgUrl: "/assets/images/products/categories/sandal.png"
-            },
-            {
-              title: "",
-              href: "/product/search/Formal",
-              imgUrl: "/assets/images/products/categories/shirt.png"
-            },
-            {
-              title: "",
-              href: "/product/search/Casual",
-              imgUrl: "/assets/images/products/categories/t-shirt.png"
-            }
-          ]
-        },
-        {
-          title: "",
-          href: "/product/search/bags",
-          subCategories: [
-            {
-              title: "",
-              href: "/product/search/backpack",
-              imgUrl: "/assets/images/products/categories/backpack.png"
-            },
-            {
-              title: "",
-              href: "/product/search/Crossbody Bags",
-              imgUrl: "/assets/images/products/categories/bag.png"
-            },
-            {
-              title: "",
-              href: "/product/search/Side Bags",
-              imgUrl: "/assets/images/products/categories/mini-bag.png"
-            },
-            {
-              title: "",
-              href: "/product/search/Slides",
-              imgUrl: "/assets/images/products/categories/belt.png"
-            }
-          ]
-        }
-      ]
-    }
+    showCaret: false,
+    menuData: null
   },
   {
     icon: "brand_awareness",
     title: "Media",
     href: "/media",
-    menuComponent: "MegaMenu2",
-    menuData: [
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/fashion"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/electronics"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/home&garden"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/bikes"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/gifts"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/music"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/health&beauty"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/pets"
-      }
-    ]
+    menuComponent: "MegaMenu1",
+    showCaret: false,
+    menuData: null
   },
   {
     icon: "crowdsource",
     title: "Investment",
     href: "/product/search/gifts",
-    menuComponent: "MegaMenu2",
-    menuData: [
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/fashion"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/electronics"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/home&garden"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/bikes"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/gifts"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/music"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/health&beauty"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/pets"
-      }
-    ]
+    menuComponent: "MegaMenu1",
+    showCaret: false,
+    menuData: null
   },
   {
     icon: "calendar_month",
     title: "Calendar",
     href: "/product/search/gifts",
-    menuComponent: "MegaMenu2",
-    menuData: [
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/fashion"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/electronics"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/home&garden"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/bikes"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/gifts"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/music"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/health&beauty"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/pets"
-      }
-    ]
+    menuComponent: "MegaMenu1",
+    showCaret: false,
+    menuData: null
   },
   {
     icon: "rocket_launch",
     title: "Opportunities",
     href: "/product/search/gifts",
-    menuComponent: "MegaMenu2",
-    menuData: [
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/fashion"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/electronics"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/home&garden"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/bikes"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/gifts"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/music"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/health&beauty"
-      },
-      {
-        icon: "",
-        title: "",
-        href: "/product/search/pets"
-      }
-    ]
+    menuComponent: "MegaMenu1",
+    showCaret: false,
+    menuData: null
   },
 ];
 

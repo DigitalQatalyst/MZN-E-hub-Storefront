883ce2c837f4d971d7c77b0e8a39e140f70de7bd
Digitalqatalyst <dq.demo.a@digitalqatalyst.com>
2025-09-04 20:32:24 +0300

    feature_doanloads

--- src/components/documentwallet/MyDownloads.tsx
+++ src/components/documentwallet/MyDownloads.tsx
@@ -1,10 +1,451 @@
+import Grid from "@component/grid/Grid";
+import { Box, Typography } from "@mui/material";
+import {
+  DotSquare,
+  EllipsisVertical,
+  File,
+  FunnelIcon,
+  Trash,
+  UploadCloud,
+} from "lucide-react";
 import React from "react";
+import { BiVerticalCenter } from "react-icons/bi";
+import { files } from "./files";
 
 const MyDownloads = () => {
   return (
-    <div>
-      <h1>My Downloads</h1>
-    </div>
+    <Box>
+      <Box
+        sx={{
+          paddingY: "24px",
+        }}
+      >
+        <Typography
+          sx={{
+            color: "#000",
+            textAlign: "start",
+            fontFeatureSettings: "liga,clig",
+            fontFamily: "Inter",
+            fontSize: "24px",
+            fontStyle: "normal",
+            fontWeight: 500,
+            lineHeight: "22px",
+          }}
+        >
+          Downloads
+        </Typography>
+      </Box>
+
+      {/* search area */}
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingY: "24px",
+        }}
+      >
+        <Box>
+          <input
+            type="text"
+            placeholder="Search files by name"
+            style={{
+              padding: "8px 16px",
+              borderRadius: "8px",
+              border: "1px solid divider",
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
+          <FunnelIcon size={15} color="grey" />
+          <Box>
+            <select
+              style={{
+                padding: "6px 12px",
+                borderRadius: "8px",
+                border: "1px solid divider",
+              }}
+            >
+              <option value="all">All</option>
+              <option value="pdf">PDF</option>
+              <option value="docx">DOCX</option>
+              <option value="images">Images</option>
+            </select>
+          </Box>
+        </Box>
+      </Box>
+
+      {/* my files area */}
+      <Box
+        sx={{
+          width: "100%",
+          height: "auto",
+          paddingY: "12px",
+        }}
+      >
+        <Typography
+          sx={{
+            fontFamily: "Inter",
+            fontSize: "16px",
+            fontStyle: "normal",
+            fontWeight: 500,
+            lineHeight: "22px",
+          }}
+        >
+          My Files
+        </Typography>
+
+        <Box
+          sx={{
+            paddingY: "12px",
+            width: "100%",
+          }}
+        >
+          {/* grid */}
+          <Grid container spacing={6} gap={6}>
+            {files.map((file) => {
+              return (
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <Box
+                    sx={{
+                      width: "100%",
+                      height: "250px",
+                      backgroundColor: "#fff",
+                      borderRadius: "12px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      flexDirection: "column",
+                      gap: "12px",
+                      position: "relative",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        position: "absolute",
+                        top: "12px",
+                        right: "12px",
+                        cursor: "pointer",
+                      }}
+                    >
+                      <EllipsisVertical size={20} color="grey" />
+                    </Box>
+                    <Box
+                      sx={{
+                        padding: "12px",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                        gap: "12px",
+                        height: "100px",
+                        width: "100px",
+                        borderRadius: "24px",
+                        backgroundColor: "#D9EEFF",
+                      }}
+                    >
+                      {/* svg file */}
+                      <img
+                        src={
+                          file?.type === "document"
+                            ? "/images/news.svg"
+                            : "/images/photo.svg"
+                        }
+                        width={64}
+                        height={64}
+                        alt=""
+                      />
+                    </Box>
+                    <Typography
+                      sx={{
+                        fontFamily: "Inter",
+                        fontSize: "16px",
+                        fontStyle: "normal",
+                        fontWeight: 500,
+                        lineHeight: "22px",
+                        color: "#00140E",
+                      }}
+                    >
+                      {file?.name}
+                    </Typography>
+                    <Typography
+                      sx={{
+                        fontFamily: "Inter",
+                        fontSize: "12px",
+                        fontStyle: "normal",
+                        fontWeight: 300,
+                        textAlign: "center",
+                        lineHeight: "22px",
+                        color: "#00140E",
+                      }}
+                    >
+                      {file?.date} - {file?.size}
+                    </Typography>
+                  </Box>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Box>
+      </Box>
+
+      {/* recent activity area */}
+      <Box
+        sx={{
+          paddingY: "24px",
+        }}
+      >
+        <Typography
+          sx={{
+            color: "#000",
+            textAlign: "start",
+            fontFeatureSettings: "liga,clig",
+            fontFamily: "Inter",
+            fontSize: "24px",
+            fontStyle: "normal",
+            fontWeight: 500,
+            lineHeight: "22px",
+          }}
+        >
+          Folders
+        </Typography>
+        <Typography
+          sx={{
+            color: "#000",
+            textAlign: "start",
+            fontFeatureSettings: "liga,clig",
+            fontFamily: "Inter",
+            fontSize: "16px",
+            fontStyle: "normal",
+            fontWeight: 400,
+            lineHeight: "22px",
+            marginTop: "16px",
+          }}
+        >
+          Group your uploaded files into folders
+        </Typography>
+      </Box>
+
+      {/* my files area */}
+      <Box
+        sx={{
+          width: "100%",
+          height: "auto",
+          paddingY: "12px",
+        }}
+      >
+        <Box
+          sx={{
+            paddingY: "12px",
+            width: "100%",
+          }}
+        >
+          <Grid container spacing={6} gap={6}>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <Box
+                sx={{
+                  width: "100%",
+                  height: "250px",
+                  backgroundColor: "#fff",
+                  borderRadius: "12px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                  gap: "12px",
+                  position: "relative",
+                }}
+              >
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "12px",
+                    right: "12px",
+                    cursor: "pointer",
+                  }}
+                >
+                  <EllipsisVertical size={20} color="grey" />
+                </Box>
+                <Box
+                  sx={{
+                    padding: "12px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    gap: "12px",
+                    height: "100px",
+                    width: "100px",
+                    borderRadius: "24px",
+                    backgroundColor: "#D9EEFF",
+                  }}
+                >
+                  {/* svg file */}
+                  <img src="/images/folder.svg" width={64} height={64} alt="" />
+                </Box>
+                <Typography
+                  sx={{
+                    fontFamily: "Inter",
+                    fontSize: "16px",
+                    fontStyle: "normal",
+                    fontWeight: 500,
+                    lineHeight: "22px",
+                    color: "#00140E",
+                  }}
+                >
+                  Reports
+                </Typography>
+                <Typography
+                  sx={{
+                    fontFamily: "Inter",
+                    fontSize: "12px",
+                    fontStyle: "normal",
+                    fontWeight: 300,
+                    textAlign: "center",
+                    lineHeight: "22px",
+                    color: "#00140E",
+                  }}
+                >
+                  Created on Jun 23, 2024
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <Box
+                sx={{
+                  width: "100%",
+                  height: "250px",
+                  backgroundColor: "#fff",
+                  borderRadius: "12px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                  gap: "12px",
+                  position: "relative",
+                }}
+              >
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "12px",
+                    right: "12px",
+                    cursor: "pointer",
+                  }}
+                >
+                  <EllipsisVertical size={20} color="grey" />
+                </Box>
+                <Box
+                  sx={{
+                    padding: "12px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    gap: "12px",
+                    height: "100px",
+                    width: "100px",
+                    borderRadius: "24px",
+                    backgroundColor: "#D9EEFF",
+                  }}
+                >
+                  {/* svg file */}
+                  <img src="/images/folder.svg" width={64} height={64} alt="" />
+                </Box>
+                <Typography
+                  sx={{
+                    fontFamily: "Inter",
+                    fontSize: "16px",
+                    fontStyle: "normal",
+                    fontWeight: 500,
+                    lineHeight: "22px",
+                    color: "#00140E",
+                  }}
+                >
+                  Form Documents
+                </Typography>
+                <Typography
+                  sx={{
+                    fontFamily: "Inter",
+                    fontSize: "12px",
+                    fontStyle: "normal",
+                    fontWeight: 300,
+                    textAlign: "center",
+                    lineHeight: "22px",
+                    color: "#00140E",
+                  }}
+                >
+                  Created on Jun 27, 2024
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <Box
+                sx={{
+                  width: "100%",
+                  height: "200px",
+                  backgroundColor: "#fff",
+                  borderRadius: "12px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                  gap: "12px",
+                  position: "relative",
+                }}
+              >
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "12px",
+                    right: "12px",
+                    cursor: "pointer",
+                  }}
+                >
+                  <EllipsisVertical size={20} color="grey" />
+                </Box>
+                <Box
+                  sx={{
+                    padding: "12px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    gap: "12px",
+                    height: "100px",
+                    width: "100px",
+                    borderRadius: "24px",
+                    backgroundColor: "#D9EEFF",
+                  }}
+                >
+                  {/* svg file */}
+                  <img src="/images/add.svg" width={64} height={64} alt="" />
+                </Box>
+                <Typography
+                  sx={{
+                    fontFamily: "Inter",
+                    fontSize: "16px",
+                    fontStyle: "normal",
+                    fontWeight: 500,
+                    lineHeight: "22px",
+                    color: "#00140E",
+                  }}
+                >
+                  Create Folder
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
--- src/components/documentwallet/files.tsx
+++ src/components/documentwallet/files.tsx
@@ -17,7 +17,115 @@ type Uploads = {
   size: string;
 };
 
+type Downloads = {
+  id: number;
+  name: string;
+  date: string;
+  icon: string;
+  type: string;
+  size: string;
+};
+
 export const files: File[] = [
+  {
+    id: 1,
+    name: "Completion_Certif..",
+    date: "May 20, 2024",
+    icon: "/images/news.svg",
+    type: "document",
+    size: "800 KB",
+  },
+  {
+    id: 2,
+    name: "Financials_Q1.rep...",
+    date: "May 25, 2024",
+    icon: "/images/news.svg",
+    type: "document",
+    size: "1.1 MB",
+  },
+  {
+    id: 3,
+    name: "Annual_Report...",
+    date: "May 30, 2024",
+    icon: "/images/news.svg",
+    type: "document",
+    size: "2.5 MB",
+  },
+  {
+    id: 4,
+    name: "Annual_Report...",
+    date: "May 30, 2024",
+    icon: "/images/news.svg",
+    type: "document",
+    size: "2.5 MB",
+  },
+  {
+    id: 5,
+    name: "Annual_Report...",
+    date: "Jun 05, 2024",
+    icon: "/images/news.svg",
+    type: "document",
+    size: "2.5 MB",
+  },
+  {
+    id: 6,
+    name: "Project_Propos...",
+    date: "Jun 10, 2024",
+    icon: "/images/news.svg",
+    type: "document",
+    size: "512KB",
+  },
+  {
+    id: 7,
+    name: "Project_Propos...",
+    date: "Jun 10, 2024",
+    icon: "/images/news.svg",
+    type: "document",
+    size: "512KB",
+  },
+  {
+    id: 8,
+    name: "Team_Photo.jpg",
+    date: "Jun 12, 2024",
+    icon: "/images/news.svg",
+    type: "image",
+    size: "3.1 MB",
+  },
+  {
+    id: 9,
+    name: "Logo_Design.jpg",
+    date: "Jun 21, 2024",
+    icon: "/images/news.svg",
+    type: "image",
+    size: "3.1 MB",
+  },
+  {
+    id: 10,
+    name: "Annual_Report...",
+    date: "Jun 23, 2024",
+    icon: "/images/news.svg",
+    type: "document",
+    size: "2.5 MB",
+  },
+  {
+    id: 11,
+    name: "Annual_Report...",
+    date: "Jun 29, 2024",
+    icon: "/images/news.svg",
+    type: "document",
+    size: "2.5 MB",
+  },
+  {
+    id: 12,
+    name: "Annual_Report...",
+    date: "Jun 29, 2024",
+    icon: "/images/news.svg",
+    type: "document",
+    size: "2.5 MB",
+  },
+];
+
+export const downloads: Downloads[] = [
   {
     id: 1,
     name: "Completion_Certif..",

c2f3adcd4a30b24e57e67a051c7d4573a533f960
DigitalQatalyst <dq.demo.a@digitalqatalyst.com>
2025-09-05 22:19:51 +0300

    resolve build error

--- src/app/(layout-1)/services/page.tsx
+++ src/app/(layout-1)/services/page.tsx
@@ -1,56 +1,24 @@
-"use client"
-import { Fragment } from "react";
-// GLOBAL CUSTOM COMPONENTS
+"use client";
+
+import { Fragment, useState } from "react";
 import Box from "@component/Box";
 import NavbarMarketplace from "@component/navbar/NavbarMarketplace";
-// PAGE SECTION COMPONENTS
-
-import Section6 from "@sections/market-2/section-6/Section6";
-
+import FinancialServiceCatalogue from "@sections/market-2/financial-service-catalogue";
+import Navbar from "@component/navbar/marketplace_nav/Navbar";
 
 export default function MarketTwo() {
+  const [activeButton, setActiveButton] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <Fragment>
       {/* NAVBAR AREA */}
-      <NavbarMarketplace />
+      <Navbar />
 
       <Box bg="#F6F6F6">
-        {/* HERO CAROUSEL AREA */}
-        {/* <Section1 /> */}
-      
-        {/* SERVICE LIST AREA --- To be results section*/}
-          {/* <Section2 /> */}
-
-          {/* TOP CATEGORIES AREA */}
-          {/* <Section3 /> */}
-
-          {/* DEAL OF THE DAY PRODUCTS AREA */}
-          {/* <Section4 /> */}
-
-          {/* NEW ARRIVALS AND BEST SELLER OFFER BANNER AREA */}
-          {/* <Section5 /> */}
-
-          {/* ELECTRONICS CATEGORY BASED PRODUCTS AREA */}
-          <Section6 />
-
-          {/* SALES OFFER BANNERS AREA */}
-          {/* <Section7 /> */}
-
-          {/* MEN'S CATEGORY BASED PRODUCTS AREA */}
-          {/* <Section6 data={[]} /> */} {/* Empty data passed */}
-
-          {/* DISCOUNT OFFER BANNER AREA */}
-          {/* <Section8 /> */}
-
-          {/* WOMEN'S CATEGORY BASED PRODUCTS AREA */}
-          {/* <Section6 data={[]} /> */} {/* Empty data passed */}
-
-          {/* FEATURES BRAND LIST AREA */}
-          {/* <Section9 brands={[]} /> */} {/* Empty data passed */}
-
-          {/* SELECTED PRODUCTS AREA */}
-          {/* <Section10 products={[]} /> */} {/* Empty data passed */}
         
+        <FinancialServiceCatalogue activeButton={activeButton} setActiveButton={setActiveButton} />
+
       </Box>
     </Fragment>
   );
